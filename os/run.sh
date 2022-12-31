f178b2c2267180deffad8007c7efbae360c2f3c5
thomation <thomation@126.com>
2022-12-31 19:04:30 +0800

    run on riscv32

--- os/run.sh
+++ os/run.sh
@@ -1,1 +1,2 @@
-qemu-system-riscv64 -nographic -smp 1 -machine virt -bios none -kernel os.elf -s -S &
+#qemu-system-riscv32 -nographic -smp 1 -machine virt -bios none -kernel os.elf -s -S &
+qemu-system-riscv32 -nographic -smp 1 -machine virt -bios none -kernel os.elf
