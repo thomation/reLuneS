29bb5aa1718abf4708eef2f207ead251f4ec55ee
thomation <thomation@126.com>
2023-01-01 10:34:12 +0800

    Add page

--- os/kernel.c
+++ os/kernel.c
@@ -1,10 +1,13 @@
 #include "os.h"
 
 extern void uart_init(void);
+extern void page_init(void);
 
 void start_kernel()
 {
     uart_init();
     uart_puts("Hello reLune\n");
+    page_init();
+
     while(1) {}
 }
