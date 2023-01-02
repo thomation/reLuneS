b7f52648ae5722302d01add3edf5b91b2270c0e5
thomation <thomation@126.com>
2023-01-02 09:59:21 +0800

    Multiple tasks

--- os/kernel.c
+++ os/kernel.c
@@ -4,6 +4,8 @@ extern void uart_init(void);
 extern void page_init(void);
 extern void sched_init(void);
 extern void schedule(void);
+extern void os_main(void);
+
 void start_kernel()
 {
     uart_init();
@@ -11,6 +13,7 @@ void start_kernel()
     page_init();
     uart_puts("Init Sched\n");
     sched_init();
+    os_main();
     schedule();
     uart_puts("Would not go here!\n");
 
--- os/sched.c
+++ os/sched.c
@@ -3,37 +3,50 @@
 /* defined in entry.S */
 extern void switch_to(struct context *next);
 
+#define MAX_TASKS 10
 #define STACK_SIZE 1024
-uint8_t task_stack[STACK_SIZE];
-struct context ctx_task;
+
+uint8_t task_stack[MAX_TASKS][STACK_SIZE];
+struct context ctx_tasks[MAX_TASKS];
+
+static int _top = 0;
+static int _current = -1;
 
 static void w_mscratch(reg_t x)
 {
 	asm volatile("csrw mscratch, %0" : : "r" (x));
 }
-void user_task0(void);
 void sched_init()
 {
 	w_mscratch(0);
-
-	ctx_task.sp = (reg_t) &task_stack[STACK_SIZE - 1];
-	ctx_task.ra = (reg_t) user_task0;
 }
 void schedule()
 {
-	struct context *next = &ctx_task;
+    if (_top <= 0) {
+		panic("Num of task should be greater than zero!");
+		return;
+	}
+    _current = (_current + 1) % _top;
+	struct context *next = &(ctx_tasks[_current]);
 	switch_to(next);
+}
+int task_create(void (*start_routin)(void))
+{
+	if (_top < MAX_TASKS) {
+		ctx_tasks[_top].sp = (reg_t) &task_stack[_top][STACK_SIZE - 1];
+		ctx_tasks[_top].ra = (reg_t) start_routin;
+		_top++;
+		return 0;
+	} else {
+		return -1;
+	}
+}
+void task_yield()
+{
+	schedule();
 }
 void task_delay(volatile int count)
 {
 	count *= 50000;
 	while (count--);
-}
-void user_task0(void)
-{
-	uart_puts("Task 0: Created!\n");
-	while (1) {
-		uart_puts("Task 0: Running...\n");
-		task_delay(1000);
-	}
 }
