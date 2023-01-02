003f5c142dc4d811c4564b16e7c6026c86638677
thomation <thomation@126.com>
2023-01-02 16:15:24 +0800

    Add trap

--- os/kernel.c
+++ os/kernel.c
@@ -5,12 +5,15 @@ extern void page_init(void);
 extern void sched_init(void);
 extern void schedule(void);
 extern void os_main(void);
+extern void trap_init(void);
 
 void start_kernel()
 {
     uart_init();
     uart_puts("Hello reLune\n");
     page_init();
+    trap_init();
+
     uart_puts("Init Sched\n");
     sched_init();
     os_main();
---
+++ os/trap.c
@@ -0,0 +1,42 @@
+#include "os.h"
+
+extern void trap_vector(void);
+
+void trap_init()
+{
+	/*
+	 * set the trap-vector base-address for machine-mode
+	 */
+	w_mtvec((reg_t)trap_vector);
+}
+
+reg_t trap_handler(reg_t epc, reg_t cause)
+{
+	reg_t return_pc = epc;
+	reg_t cause_code = cause & 0xfff;
+	
+	if (cause & 0x80000000) {
+		/* Asynchronous trap - interrupt */
+		switch (cause_code) {
+		case 3:
+			uart_puts("software interruption!\n");
+			break;
+		case 7:
+			uart_puts("timer interruption!\n");
+			break;
+		case 11:
+			uart_puts("external interruption!\n");
+			break;
+		default:
+			uart_puts("unknown async exception!\n");
+			break;
+		}
+	} else {
+		/* Synchronous trap - exception */
+		printf("Sync exceptions!, code = %d\n", cause_code);
+		panic("OOPS! What can I do!");
+		//return_pc += 4;
+	}
+
+	return return_pc;
+}
