51c3c8c727771f5ef289321bf1b0936fc2e5e778
thomation <thomation@126.com>
2023-01-02 09:27:55 +0800

    Swtich context

--- os/kernel.c
+++ os/kernel.c
@@ -2,12 +2,17 @@
 
 extern void uart_init(void);
 extern void page_init(void);
-
+extern void sched_init(void);
+extern void schedule(void);
 void start_kernel()
 {
     uart_init();
     uart_puts("Hello reLune\n");
     page_init();
+    uart_puts("Init Sched\n");
+    sched_init();
+    schedule();
+    uart_puts("Would not go here!\n");
 
     while(1) {}
 }
